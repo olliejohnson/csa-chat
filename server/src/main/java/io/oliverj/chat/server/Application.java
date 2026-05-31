f923b4450e90e1118a6d46be6d469d82b83fb14a
olliejohnson <oliver.joseph.johnson@gmail.com>
2026-05-31 16:57:20 -0400

    removed unused code

--- server/src/main/java/io/oliverj/chat/server/Application.java
+++ server/src/main/java/io/oliverj/chat/server/Application.java
@@ -1,9 +1,6 @@
 package io.oliverj.chat.server;
 
 import io.oliverj.chat.server.network.ChatServer;
-import io.oliverj.chat.server.util.HibernateUtil;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
 
 public class Application {
     public static void main(String[] args) throws Exception {
